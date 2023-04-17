3b5458258d992adf7597824a69446a5bf3a85478
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-04-17 17:00:11 +0100

    write test for statement method format

---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,15 @@
+require "bank_account"
+
+describe BankAccount do
+  it "statement prints with correct format" do
+    io = double :kernel
+    account = BankAccount.new()
+    expect(io).to receive(:puts).with("date || credit || debit || balance")
+    account.statement()
+  end
+
+  # it "makes deposits with time and date" do
+  #   account = BankAccount.new()
+  #   account.deposit()
+  # end
+end

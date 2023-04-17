ef7b3fe111227de90758247432b7c94d1807ff5f
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-04-17 18:27:50 +0100

    refactor code

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,8 @@
-require 'date'
 
 class BankAccount
   attr_accessor :transactions
 
+  # Initialize BankAccount with an IO object for mocking
   def initialize(kernel)
     @io = kernel
     @transactions = []
@@ -16,33 +16,32 @@ class BankAccount
     @transactions << {amount: -amount, date: date}
   end
 
-
+  # Print a statement of all transactions in the account
   def statement
+    # Create a copy of the transactions array so we don't modify the original
     transactions_copy = @transactions.dup
     sorted_transactions = transactions_copy.sort_by { |t| t[:date] }
     
     balance = 0
     @io.puts "date || credit || debit || balance"
     
+    # Create an array to hold formatted transaction strings
     transaction_strings = []
 
     sorted_transactions.each do |transaction|
       balance += transaction[:amount]
+
+      # Format transaction string based on transaction type (deposit or withdrawal)
       if transaction[:amount] > 0
         transaction_strings << "#{transaction[:date]} || #{'%.2f' % transaction[:amount]} || || #{'%.2f' % balance}"
       else
         transaction_strings << "#{transaction[:date]} || || #{'%.2f' % -transaction[:amount]} || #{'%.2f' % balance}"
       end
     end
+
+    # Print transaction strings in reverse order (latest transactions first)
     transaction_strings.reverse_each do |t|
       @io.puts t
     end
   end
 end
-
-# account = BankAccount.new(Kernel)
-
-# account.withdraw(500, "14/01/2021")
-# account.deposit(2000, "15/01/2021")
-# account.deposit(1000, "10/01/2021")
-# account.statement
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,13 +1,6 @@
 require "bank_account"
 
 describe BankAccount do
-  it "statement prints with correct format" do
-    io = double :kernel
-    account = BankAccount.new(io)
-    expect(io).to receive(:puts).with("date || credit || debit || balance")
-    account.statement()
-  end
-
   it "makes deposits with amount and date" do
     account = BankAccount.new(Kernel)
     account.deposit(1000, "01/10/2021")
@@ -20,16 +13,25 @@ describe BankAccount do
     expect(account.transactions).to eq [{amount: -500, date:"01/10/2021"}] 
   end
 
-  it "statement shows with all transactions" do
-    io = double :kernel
-    account = BankAccount.new(io)
-    account.deposit(1000, "01/10/2021")
-    account.deposit(2000, "05/10/2021")
-    account.withdraw(500, "06/10/2021")
-    expect(io).to receive(:puts).with("date || credit || debit || balance")
-    expect(io).to receive(:puts).with("06/10/2021 || || 500.00 || 2500.00")
-    expect(io).to receive(:puts).with("05/10/2021 || 2000.00 || || 3000.00")
-    expect(io).to receive(:puts).with("01/10/2021 || 1000.00 || || 1000.00")
-    account.statement()
+  context "statement method" do
+    it "statement prints with correct format" do
+      io = double :kernel
+      account = BankAccount.new(io)
+      expect(io).to receive(:puts).with("date || credit || debit || balance")
+      account.statement()
+    end
+
+    it "statement shows all transactions" do
+      io = double :kernel
+      account = BankAccount.new(io)
+      account.deposit(2000, "05/10/2021")
+      account.deposit(1000, "01/10/2021")
+      account.withdraw(500, "06/10/2021")
+      expect(io).to receive(:puts).with("date || credit || debit || balance")
+      expect(io).to receive(:puts).with("06/10/2021 || || 500.00 || 2500.00")
+      expect(io).to receive(:puts).with("05/10/2021 || 2000.00 || || 3000.00")
+      expect(io).to receive(:puts).with("01/10/2021 || 1000.00 || || 1000.00")
+      account.statement()
+    end
   end
 end
